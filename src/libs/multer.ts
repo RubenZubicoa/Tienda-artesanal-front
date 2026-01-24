a08369eb89587006d499399f0bd1550270b7b214
Ruben <rzubicoa@eservicios.indra.es>
2026-01-24 12:51:23 +0100

    chore: eliminar la libreria uuid

--- src/libs/multer.ts
+++ src/libs/multer.ts
@@ -1,11 +1,10 @@
 import multer from 'multer';
-import uuid from 'uuid';
 import path from 'path';
 
 const storage = multer.diskStorage({
     destination: 'uploads',
     filename: (req, file, cb) =>{
-        cb(null, uuid.v4() + path.extname(file.originalname));
+        cb(null, Date.now().toString() + path.extname(file.originalname));
     }
 });
 
