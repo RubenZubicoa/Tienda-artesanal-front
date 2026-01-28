d4d42fc1710eec79949a7ff997bdc0f5b2a17b2f
Ruben <rzubicoa@eservicios.indra.es>
2026-01-28 16:51:59 +0100

    fix: corregir error de url en environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://tienda-artesanal-back-produccion.vercel.app/api'
+    apiUrl: 'https://tienda-back-produccion.vercel.app/api'
   };
   
