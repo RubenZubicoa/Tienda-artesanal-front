538f53b4b82e8bf3f45d93e1dd07c53509af6905
Ruben <rzubicoa@eservicios.indra.es>
2026-03-18 18:40:41 +0100

    feat: mejorar la busqueda por ubicacion

--- src/app/layout/components/header/search-bar/search-bar.component.ts
+++ src/app/layout/components/header/search-bar/search-bar.component.ts
@@ -1,10 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, DestroyRef, inject } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { LocationService } from '../../../../core/services/location.service';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { TranslatePipe } from '@ngx-translate/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
@@ -14,6 +16,18 @@ import { TranslatePipe } from '@ngx-translate/core';
 })
 export class SearchBarComponent {
   private readonly locationService = inject(LocationService);
+  private readonly destroyRef = inject(DestroyRef);
+
+  public addressFormControl = new FormControl('');
+
+  constructor() {
+    this.addressFormControl.valueChanges.pipe(debounceTime(600), takeUntilDestroyed(this.destroyRef)).subscribe((value) => {
+      if (value) {
+        this.changeLocation(value);
+      }
+    });
+  }
+
 
   public changeLocation(address: string) {
     if (address) {
